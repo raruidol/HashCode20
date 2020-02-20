0dca8513412226e429e7150b3a7ef84febc002c8
cardieal <>
2020-02-20 19:13:31 +0100

    lectura datos

--- entrada.py
+++ entrada.py
@@ -2,8 +2,28 @@
 def cargar_datos(fichero):
     datos = {}
     with open(fichero) as f:
-        for i in range(10):
-            print(f.readline())
+        l = f.readline().split(" ")
+        numero_libros = int(l[0])
+        numero_librerias = int(l[1])
+        datos["numero_dias"] = int(l[2])
+        datos["librerias"] = {}
+        datos["scores"] = [int(x) for x in f.readline().split(" ")]
+
+        for libreria in range(numero_librerias):
+            linea1 = [int(x) for x in f.readline().split(" ")]
+            datos["librerias"][libreria] = {}
+            datos["librerias"][libreria]["numero_libros"] = linea1[0]
+            datos["librerias"][libreria]["signup"] = linea1[1]
+            datos["librerias"][libreria]["libros_dia"] = linea1[2]
+
+            datos["librerias"][libreria]["libros"] = [int(x) for x in f.readline().split(" ")]
+
+
+
+
+
+    print(datos)
+
 
 if __name__ == '__main__':
-    cargar_datos("README.md")
+    cargar_datos("data/a_example.txt")
